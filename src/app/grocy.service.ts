36d8d51eeaff34b7553201b473c06b5198bd9583
Marco Franke <mfranke87@icloud.com>
2023-12-17 11:13:51 +0100

    feat: first release ✨

--- src/app/grocy.service.ts
+++ src/app/grocy.service.ts
@@ -1,93 +1,131 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, map, tap } from 'rxjs';
-import { Recipe } from './interfaces/recipe.interface';
-import { Meal } from './interfaces/meal.interface';
+import {
+  BehaviorSubject,
+  Observable,
+  switchMap,
+  filter,
+  take,
+  tap,
+  map,
+} from 'rxjs';
+import { AppConfigService } from './appconfig.service'; // Adjust this import based on your actual file paths
 import { MealPlanSection } from './interfaces/meal-plan-section.interface';
-import { AppConfigService } from './appconfig.service';
+import { Meal } from './interfaces/meal.interface';
+import { Recipe } from './interfaces/recipe.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GrocyService {
-
-  private GROCY_URL: string | undefined ;
+  private GROCY_URL: string | undefined;
   private GROCY_HEADER: Object | undefined;
-
   private mealPlanSubject = new BehaviorSubject<Array<Partial<Meal>>>([]);
 
-  constructor(private httpClient: HttpClient, private appConfigService: AppConfigService) { 
-    this.appConfigService.isAppConfigured$.subscribe(configured => {
-
-      if(configured){
-        console.log("configured", configured)
-
+  constructor(
+    private httpClient: HttpClient,
+    private appConfigService: AppConfigService
+  ) {
+    this.appConfigService.isAppConfigured$.subscribe((configured) => {
+      if (configured) {
         const appConfig = this.appConfigService.getConfig();
-
-        debugger;
-        
-        if(appConfig.grocyApiKey && appConfig.grocyUrl){
-          this.GROCY_URL = `${this.adjustTrailingSlash(appConfig.grocyUrl || '')}/api/`;
-          this.GROCY_HEADER = {
-            headers: { 'GROCY-API-KEY': appConfig.grocyApiKey?.trim() }
-          };
-    
-          this.loadMealPlan()
-    
-          console.log("GROCY_URL", this.GROCY_URL);      
-          console.log("GROCY_HEADER", this.GROCY_HEADER);      
-        }
+        this.GROCY_URL = this.adjustUrl(appConfig.grocyUrl || '');
+        this.GROCY_HEADER = {
+          headers: { 'GROCY-API-KEY': appConfig.grocyApiKey?.trim() },
+        };
+        this.loadMealPlan();
       }
-
-     
     });
   }
 
-  private adjustTrailingSlash(url: string) {
-    if (url.endsWith('/')) {
-      
-        return url.slice(0, -1);
-      } else {
-        return url;
-    }
-}
+  private adjustUrl(url: string): string {
+    return url.endsWith('/') ? url.slice(0, -1) + '/api/' : url + '/api/';
+  }
+
+  private waitForConfiguration<T>(): Observable<boolean> {
+    return this.appConfigService.isAppConfigured$.pipe(
+      filter((configured) => configured),
+      take(1)
+    );
+  }
+
+  private httpRequest<T>(requestFn: () => Observable<T>): Observable<T> {
+    return this.waitForConfiguration().pipe(switchMap(() => requestFn()));
+  }
 
   private loadMealPlan() {
-    this.httpClient.get<Array<Partial<Meal>>>(`${this.GROCY_URL}objects/meal_plan`, this.GROCY_HEADER)
-      .subscribe(mealPlan => this.mealPlanSubject.next(mealPlan));
+    const request = () =>
+      this.httpClient.get<Array<Partial<Meal>>>(
+        `${this.GROCY_URL}objects/meal_plan`,
+        this.GROCY_HEADER
+      );
+    this.httpRequest(request).subscribe((mealPlan) =>
+      this.mealPlanSubject.next(mealPlan)
+    );
   }
 
   getMealPlan(): Observable<Array<Partial<Meal>>> {
     return this.mealPlanSubject.asObservable();
   }
 
-  postMeal(meal: Partial<Meal>){
-    return this.httpClient.post<{ created_object_id: string }>(`${this.GROCY_URL}objects/meal_plan`, meal, this.GROCY_HEADER)
-      .pipe(tap(() => this.loadMealPlan()));
+  postMeal(meal: Partial<Meal>): Observable<any> {
+    const request = () =>
+      this.httpClient.post<{ created_object_id: string }>(
+        `${this.GROCY_URL}objects/meal_plan`,
+        meal,
+        this.GROCY_HEADER
+      );
+    return this.httpRequest(request).pipe(tap(() => this.loadMealPlan()));
   }
 
-  deleteMeal(mealId: number){
-    return this.httpClient.delete(`${this.GROCY_URL}objects/meal_plan/${mealId}`, this.GROCY_HEADER)
-      .pipe(tap(() => this.loadMealPlan()));
+  deleteMeal(mealId: number): Observable<any> {
+    const request = () =>
+      this.httpClient.delete(
+        `${this.GROCY_URL}objects/meal_plan/${mealId}`,
+        this.GROCY_HEADER
+      );
+    return this.httpRequest(request).pipe(tap(() => this.loadMealPlan()));
   }
 
   getRecipes(): Observable<Array<Recipe>> {
-    return this.httpClient.get<Array<Recipe>>(`${this.GROCY_URL}objects/recipes`, this.GROCY_HEADER)
-      .pipe(map(recipes => recipes.filter(recipe => recipe.type == "normal")));
+    const request = () =>
+      this.httpClient.get<Array<Recipe>>(
+        `${this.GROCY_URL}objects/recipes`,
+        this.GROCY_HEADER
+      );
+    return this.httpRequest(request).pipe(
+      map((recipes) => recipes.filter((recipe) => recipe.type == 'normal'))
+    );
   }
 
-  getGrocyImage(fileGroup: string, fileName: string): Observable<string>{
-    return this.httpClient.get(`${this.GROCY_URL}files/${fileGroup}/${btoa(fileName)}`, { ...this.GROCY_HEADER, responseType: 'blob' })
-      .pipe(map(e => URL.createObjectURL(e)));
+  getGrocyImage(fileGroup: string, fileName: string): Observable<string> {
+    const request = () =>
+      this.httpClient.get(
+        `${this.GROCY_URL}files/${fileGroup}/${btoa(fileName)}`,
+        {
+          ...this.GROCY_HEADER,
+          responseType: 'blob',
+        }
+      );
+    return this.httpRequest(request).pipe(map((e) => URL.createObjectURL(e)));
   }
 
-  getMealPlanSections(): Observable<Array<MealPlanSection>>{
-    return this.httpClient.get<Array<MealPlanSection>>(`${this.GROCY_URL}objects/meal_plan_sections`, this.GROCY_HEADER);
+  getMealPlanSections(): Observable<Array<MealPlanSection>> {
+    const request = () =>
+      this.httpClient.get<Array<MealPlanSection>>(
+        `${this.GROCY_URL}objects/meal_plan_sections`,
+        this.GROCY_HEADER
+      );
+    return this.httpRequest(request);
   }
 
-  updateMeal(meal: Partial<Meal>){
-    return this.httpClient.put(`${this.GROCY_URL}objects/meal_plan/${meal.id}`, meal, this.GROCY_HEADER)
-      .pipe(tap(() => this.loadMealPlan()));
+  updateMeal(meal: Partial<Meal>): Observable<any> {
+    const request = () =>
+      this.httpClient.put(
+        `${this.GROCY_URL}objects/meal_plan/${meal.id}`,
+        meal,
+        this.GROCY_HEADER
+      );
+    return this.httpRequest(request).pipe(tap(() => this.loadMealPlan()));
   }
-  
 }
--- src/app/transloco-root.module.ts
+++ src/app/transloco-root.module.ts
@@ -1,25 +1,23 @@
-import {
-  provideTransloco,
-  TranslocoModule
-} from '@ngneat/transloco';
+import { provideTransloco, TranslocoModule } from '@ngneat/transloco';
 import { isDevMode, NgModule } from '@angular/core';
 import { TranslocoHttpLoader } from './transloco-loader';
 
-
 @NgModule({
-  exports: [ TranslocoModule ],
+  exports: [TranslocoModule],
   providers: [
-      provideTransloco({
-        config: {
-          availableLangs: ['en', 'de'],
-          // defaultLang: ["de", "en"].includes(navigator.language) ? navigator.language : "en",
-          // Remove this option if your application doesn't support changing language in runtime.
-          // reRenderOnLangChange: true,
-          fallbackLang: "en",
-          prodMode: !isDevMode(),
-        },
-        loader: TranslocoHttpLoader
-      }),
+    provideTransloco({
+      config: {
+        availableLangs: ['en', 'de'],
+        defaultLang: ['de', 'en'].includes(navigator.language)
+          ? navigator.language
+          : 'en',
+        // Remove this option if your application doesn't support changing language in runtime.
+        // reRenderOnLangChange: true,
+        // fallbackLang: "en",
+        prodMode: !isDevMode(),
+      },
+      loader: TranslocoHttpLoader,
+    }),
   ],
 })
 export class TranslocoRootModule {}
